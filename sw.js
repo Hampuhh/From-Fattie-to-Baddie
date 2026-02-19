e00fb85c9343dac64eb803a7ce68358c804edc7b
Hampuhh <43082280+Hampuhh@users.noreply.github.com>
2026-02-19 13:21:19 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'baddie-tracker-v6';
+const CACHE_NAME = 'baddie-tracker-v7'; // Cuando hagas un cambio en el futuro, solo sube este número (v8, v9...)
 const urlsToCache = [
   './',
   './index.html',
@@ -8,15 +8,34 @@ const urlsToCache = [
   './icono.png'
 ];
 
+// 1. INSTALACIÓN: Guarda los archivos nuevos
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
         return cache.addAll(urlsToCache);
       })
+      .then(() => self.skipWaiting()) // Magia: Obliga al celular a instalar la nueva versión de inmediato
   );
 });
 
+// 2. ACTIVACIÓN: Borra la basura vieja (Esto es lo que faltaba)
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Borrando caché antigua:', cacheName);
+            return caches.delete(cacheName); // Destruye la versión vieja
+          }
+        })
+      );
+    }).then(() => self.clients.claim()) // Magia: Toma el control de la pantalla al instante
+  );
+});
+
+// 3. FETCH: Muestra la app sin internet
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
